d1ccaf9f22a30f45225ec0fed12413f2359be830
Mark Canlas <github@htmlism.com>
2019-06-03 16:47:14 -0400

    second draft

--- src/main/scala/com/htmlism/Gojuon.scala
+++ src/main/scala/com/htmlism/Gojuon.scala
@@ -8,10 +8,46 @@ object Gojuon extends IOApp {
   val hiraganaCodepoint = 0x3041
   val katakanaCodepoint = 0x30A1
 
+  val vowels =
+    List(VowelA, VowelI, VowelU, VowelE, VowelO)
+
+  val consonants =
+    List(ConsonantK,
+      ConsonantS,
+      ConsonantT,
+      ConsonantN,
+      ConsonantH,
+      ConsonantM,
+      ConsonantY,
+      ConsonantR,
+      ConsonantW)
+
+  val kanaDescription =
+    vowels
+      .flatMap(v => (v, Small) :: (v, Large) :: Nil) ++
+    consonants
+      .flatMap { c =>
+        vowels
+          .flatMap { v =>
+            (c, v) match {
+              case (ConsonantT, VowelU) =>
+                (c, v, Small) :: (c, v, Unvoiced) :: (c, v, Voiced) :: Nil
+              case (ConsonantN, _) =>
+                (c, v) :: Nil
+              case (ConsonantH, _) =>
+                (c, v, Unvoiced) :: (c, v, Voiced) :: (c, v, Half) :: Nil
+              case _ =>
+                (c, v, Unvoiced) :: (c, v, Voiced) :: Nil
+            }
+          }
+      }
+
   def run(args: List[String]): IO[ExitCode] =
     IO {
       for (n <- 0 to 100) {
-        (hiraganaCodepoint + n).toChar |> println
+        println {
+          kanaDescription(n) + ": " + (hiraganaCodepoint + n).toChar
+        }
       }
 
       println
@@ -19,8 +55,36 @@ object Gojuon extends IOApp {
       println
 
       for (n <- 0 to 100) {
-        (katakanaCodepoint + n).toChar |> println
+        println {
+          kanaDescription(n) + ": " + (katakanaCodepoint + n).toChar
+        }
       }
     }.as(ExitCode.Success)
 }
 
+sealed trait Vowel
+case object VowelA extends Vowel
+case object VowelI extends Vowel
+case object VowelU extends Vowel
+case object VowelE extends Vowel
+case object VowelO extends Vowel
+
+sealed trait Consonant
+case object ConsonantK extends Consonant
+case object ConsonantS extends Consonant
+case object ConsonantT extends Consonant
+case object ConsonantN extends Consonant
+case object ConsonantH extends Consonant
+case object ConsonantM extends Consonant
+case object ConsonantY extends Consonant
+case object ConsonantR extends Consonant
+case object ConsonantW extends Consonant
+
+sealed trait VowelSize
+case object Small extends VowelSize
+case object Large extends VowelSize
+
+sealed trait Voicing
+case object Voiced extends Voicing
+case object Half extends Voicing
+case object Unvoiced extends Voicing
