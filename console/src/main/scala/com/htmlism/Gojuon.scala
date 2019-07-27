0d9884f807617bcab758f9b57764c082c51047fd
Mark Canlas <github@htmlism.com>
2019-07-27 03:55:01 -0400

    move to core

--- console/src/main/scala/com/htmlism/Gojuon.scala
+++ console/src/main/scala/com/htmlism/Gojuon.scala
@@ -4,54 +4,13 @@ import cats.effect._
 import cats.implicits._
 
 object Gojuon extends IOApp {
-  private def addVariant(f: KanaVariants => KanaVariants)(pred: PartialFunction[Kana, Unit])(kv: KanaVariants) =
-    if (pred.isDefinedAt(kv.kana))
-      f(kv)
-    else
-      kv
-
-  private[this] val kanaUnicodeDescriptions =
-    Kana
-      .allKana
-      .map(k => KanaVariants(k, hasSmall = false, hasVoiced = false, hasHalf = false))
-      .map(addVariant(_.copy(hasSmall  = true)) { case KanaCv(EmptyConsonant | ConsonantY, _) => })
-      .map(addVariant(_.copy(hasSmall  = true)) { case KanaCv(EmptyConsonant | ConsonantY, _) => })
-      .map(addVariant(_.copy(hasSmall  = true)) { case KanaCv(ConsonantT, VowelU) => })
-      .map(addVariant(_.copy(hasSmall  = true)) { case KanaCv(ConsonantW, VowelA) => })
-      .map(addVariant(_.copy(hasVoiced = true)) { case KanaCv(ConsonantH | ConsonantK | ConsonantS | ConsonantT, _) => })
-      .map(addVariant(_.copy(hasHalf   = true)) { case KanaCv(ConsonantH, _) => })
-      .flatMap { kv =>
-        val small =
-          if (kv.hasSmall)
-            List(kv.kana -> Small)
-          else
-            Nil
-
-        val voiced =
-          if (kv.hasVoiced)
-            List(kv.kana -> Voiced)
-          else
-            Nil
-
-        val half =
-          if (kv.hasHalf)
-            List(kv.kana -> Half)
-          else
-            Nil
-
-        val canonical =
-          List(kv.kana -> Canonical)
-
-        small ::: canonical ::: voiced ::: half
-      }
-
   def run(args: List[String]): IO[ExitCode] =
     IO {
-      println(s"number of kana variants is ${kanaUnicodeDescriptions.size}")
+      println(s"number of kana variants is ${Kana.kanaUnicodeDescriptions.size}")
 
-      for (n <- kanaUnicodeDescriptions.indices) {
+      for (n <- Kana.kanaUnicodeDescriptions.indices) {
         println {
-          kanaUnicodeDescriptions(n) + ": " + (Kana.hiraganaCodepoint + n).toChar
+          Kana.kanaUnicodeDescriptions(n) + ": " + (Kana.hiraganaCodepoint + n).toChar
         }
       }
 
@@ -59,9 +18,9 @@ object Gojuon extends IOApp {
       println
       println
 
-      for (n <- kanaUnicodeDescriptions.indices) {
+      for (n <- Kana.kanaUnicodeDescriptions.indices) {
         println {
-          kanaUnicodeDescriptions(n) + ": " + (Kana.katakanaCodepoint + n).toChar
+          Kana.kanaUnicodeDescriptions(n) + ": " + (Kana.katakanaCodepoint + n).toChar
         }
       }
 
--- core/src/main/scala/com/htmlism/Gojuon.scala
+++ core/src/main/scala/com/htmlism/Gojuon.scala
@@ -41,6 +41,47 @@ object Kana {
   val allKana: List[Kana] =
     cvCombinations
       .flatMap(availableKana) :+ ConsonantN
+
+  private def addVariant(f: KanaVariants => KanaVariants)(pred: PartialFunction[Kana, Unit])(kv: KanaVariants) =
+    if (pred.isDefinedAt(kv.kana))
+      f(kv)
+    else
+      kv
+
+  lazy val kanaUnicodeDescriptions: List[(Kana, Variant)] =
+    Kana
+      .allKana
+      .map(k => KanaVariants(k, hasSmall = false, hasVoiced = false, hasHalf = false))
+      .map(addVariant(_.copy(hasSmall  = true)) { case KanaCv(EmptyConsonant | ConsonantY, _) => })
+      .map(addVariant(_.copy(hasSmall  = true)) { case KanaCv(EmptyConsonant | ConsonantY, _) => })
+      .map(addVariant(_.copy(hasSmall  = true)) { case KanaCv(ConsonantT, VowelU) => })
+      .map(addVariant(_.copy(hasSmall  = true)) { case KanaCv(ConsonantW, VowelA) => })
+      .map(addVariant(_.copy(hasVoiced = true)) { case KanaCv(ConsonantH | ConsonantK | ConsonantS | ConsonantT, _) => })
+      .map(addVariant(_.copy(hasHalf   = true)) { case KanaCv(ConsonantH, _) => })
+      .flatMap { kv =>
+        val small =
+          if (kv.hasSmall)
+            List(kv.kana -> Small)
+          else
+            Nil
+
+        val voiced =
+          if (kv.hasVoiced)
+            List(kv.kana -> Voiced)
+          else
+            Nil
+
+        val half =
+          if (kv.hasHalf)
+            List(kv.kana -> Half)
+          else
+            Nil
+
+        val canonical =
+          List(kv.kana -> Canonical)
+
+        small ::: canonical ::: voiced ::: half
+      }
 }
 
 sealed trait Kana
