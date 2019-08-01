5ba0a6e3f69955bbea461789e2a82d836fb4b250
Mark Canlas <github@htmlism.com>
2019-07-31 23:53:56 -0400

    build a deck

--- anki/src/main/scala/com/htmlism/GenerateAnkiCards.scala
+++ anki/src/main/scala/com/htmlism/GenerateAnkiCards.scala
@@ -3,7 +3,13 @@ package com.htmlism
 import cats.effect._
 import cats.implicits._
 
-object GenerateAnkiCards extends GenerateAnkiCards[IO] with IOApp
+object GenerateAnkiCards extends GenerateAnkiCards[IO] with IOApp {
+  def toCard(uk: UnicodeKana): AnkiCard = {
+    val romaji = Romaji.toRomaji(uk.kana)
+
+    AnkiCard(romaji, uk.codePoint.toChar.toString, romaji)
+  }
+}
 
 class GenerateAnkiCards[F[_]](implicit F: Sync[F]) {
   def run(args: List[String]): F[ExitCode] =
@@ -25,5 +31,12 @@ class GenerateAnkiCards[F[_]](implicit F: Sync[F]) {
       .resource[F](dest)
       .map(out => out.print _)
       .map(_.compose(Serialization.deckToString))
-      .use(_ => F.unit)
+      .use(_(deck))
+
+  private lazy val deck =
+    Deck {
+      Kana
+        .buildUnicodeKana(Kana.hiraganaCodepoint, Kana.kanaVariants, Nil)
+        .map(GenerateAnkiCards.toCard)
+    }
 }
